5eea6c11340bbae5bb5ad1eacda11ef07863efa7
Jxiang-jc <zjx1020645@163.com>
2020-01-16 15:51:21 +0800

    add comment-HOC

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import AppHome from './components/pages/Home'
 import AppMore from './components/pages/More'
+import AppCommentHoc from './components/pages/common-app-hoc'
 
 import { Route, NavLink } from 'react-router-dom'
 
@@ -12,9 +13,12 @@ class App extends Component {
                 <NavLink exact to = {'/'}>首页</NavLink>
                 &nbsp;&nbsp;&nbsp;
                 <NavLink to={'/more'}>更多</NavLink>
+                &nbsp;&nbsp;&nbsp;
+                <NavLink to={'/commentHoc'}>评论appHOC</NavLink>
                 <hr/>
                 <Route exact path="/" component={AppHome}></Route>
                 <Route path="/more" component={AppMore}></Route>
+                <Route path="/commentHoc" component={AppCommentHoc}></Route>
             </div>
         )
     }
