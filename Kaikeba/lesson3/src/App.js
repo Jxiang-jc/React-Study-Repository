edf881765eaae4bcf2bdc5824cd2d2f3710308a2
Jxiang-jc <zjx1020645@163.com>
2020-01-20 21:53:16 +0800

    add

---
+++ Kaikeba/lesson3/src/App.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+import HocPage from "./pages/HocPage";
+
+function App() {
+    return (
+        <div className="App">
+            {/* <Button type="primary">Button</Button> */}
+            <HocPage />
+        </div>
+    )
+}
+
+export default App;
---
+++ Kaikeba/lesson3/src/pages/HocPage.js
@@ -0,0 +1,40 @@
+//按需加载和实现装饰器之后的页面如下：HocPage.js
+import React, { Component } from "react";
+import { Button } from "antd";
+
+const foo = Cmp => props => {
+    return (
+        <div className="border">
+            <Cmp {...props} />
+        </div>
+    );
+};
+
+const foo2 = Cmp => props => {
+    return <div className="border" style={{ border: "solid 1px red" }}>
+        <Cmp {...props} />
+    </div>;
+};
+
+@foo
+@foo2
+class Child extends Component {
+    render() {
+        return <div className="border">Child</div>
+    }
+}
+
+@foo2
+class HocPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HocPage</h1>
+                <Child />
+                <Button type="dashed">click</Button>
+            </div>
+        );
+    }
+}
+
+export default HocPage;
